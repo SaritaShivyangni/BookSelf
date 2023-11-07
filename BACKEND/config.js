51a10723f85aecd2067c4f69cddcda88dc9f61a6
Sarita <131792757+sarita1509@users.noreply.github.com>
2023-11-07 13:11:24 +0000

    "commit"

---
+++ BACKEND/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const monogoDBUL = 
+'mongodb+srv://saritashivyangni:Kissubaby@bookself.hirhvgv.mongodb.net/bookself?retryWrites=true&w=majority';
--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,97 +1,41 @@
-const express = require('express'); //import modules and packages into your application.
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express();
-const cors = require('cors'); //Cross-Origin Resource Sharing, 
-const port = process.env.PORT || 5555;
+import express from 'express';
+import { PORT, mongoDBURL } from './config.jsx';
+import mongoose from 'mongoose';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
-// middleware
-app.use(cors());
+const app = express();
+
+// Middleware for parsing request body
 app.use(express.json());
 
-// mdalmamunit427
-// arYcNetNKnJWDMGx
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
 
-
-const uri = "mongodb+srv://mdalmamunit427:arYcNetNKnJWDMGx@cluster0.lpzqr6l.mongodb.net/?retryWrites=true&w=majority";
-
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Welcome To MERN Stack Tutorial');
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+app.use('/books', booksRoute);
 
-    const database = client.db("usersDB");
-    const usersCollection = database.collection("users");
-
-    app.get('/users', async (req, res) => {
-        const cursor = usersCollection.find();
-        const result = await cursor.toArray();
-        res.send(result)
-    })
-
-    app.get('/users/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)};
-        const result = await usersCollection.findOne(query);
-        res.send(result);
-    })
-
-    app.post('/users', async (req, res) => {
-        const user = req.body;
-        const result = await usersCollection.insertOne(user);
-        res.send(result);
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
     });
-
-    app.delete('/users/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)};
-
-        const result = await usersCollection.deleteOne(query);
-        res.send(result);
-    })
-
-    app.put('/users/:id', async (req, res) => {
-        const id = req.params.id;
-        const user = req.body;
-
-        const filter = {_id: new ObjectId(id)};
-        const options = { upsert: true };
-        const updateduser = {
-            $set:{
-                name: user.name,
-                email: user.email,
-                photoURL: user.photoURL
-            }
-        }
-
-        const result = await usersCollection.updateOne(filter, updateduser, options);
-        res.send(result);
-    });
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
-}
-run().catch(console.dir);
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  })
+  .catch((error) => {
+    console.log(error);
+  });
